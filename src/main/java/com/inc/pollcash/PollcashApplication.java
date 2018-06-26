43bbbf5b1a4c5e1ba5a8f24a09fc6c27bca169d0
Misha Choe <phantomknights1@gmail.com>
2018-06-26 11:34:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/inc/pollcash/PollcashApplication.java
@@ -0,0 +1,12 @@
+package com.inc.pollcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollcashApplication.class, args);
+	}
+}
