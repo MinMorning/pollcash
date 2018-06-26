7831f3f8b696d9b82d0359d9e188d0f3ca37f3d5
Misha Choe <phantomknights1@gmail.com>
2018-06-26 12:41:47 +0900

    push

---
+++ src/main/java/com/inc/pollcash/controller/PollcashController.java
@@ -0,0 +1,14 @@
+package com.inc.pollcash.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PollcashController {
+
+	@GetMapping("/")
+	public String main(Model model) {
+		return "main";
+	}
+}
